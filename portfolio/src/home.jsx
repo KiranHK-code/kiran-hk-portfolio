03f773c3e09244b3c29768be497d394822d052b7
Kiran H K <kiranhkkiranhk60@gmail.com>
2025-12-12 19:54:30 +0530

    home

---
+++ portfolio/src/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function home() {
+  return (
+    <>
+      <div style={{display:'flex', justifyContent:'center', height:'90vh', fontSize:'24px', fontWeight:'500'}}>      
+        <p>Hi, I'm Kiran H K 👋 </p>
+      </div>
+
+    </>
+  )
+}
+
+
