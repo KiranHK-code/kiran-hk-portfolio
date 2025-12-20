f4eb5da95a353db991a75141611d0943cc5ff294
Kiran H K <kiranhkkiranhk60@gmail.com>
2025-12-20 19:33:43 +0530

    home

--- portfolio/src/home.jsx
+++ portfolio/src/home.jsx
@@ -5,6 +5,8 @@ export default function home() {
     <>
       <div style={{display:'flex', justifyContent:'center', height:'90vh', fontSize:'24px', fontWeight:'500'}}>      
         <p>Hi, I'm Kiran H K 👋 </p>
+        <p>Full Stack web/app Developer</p>
+        <p>Using React for Web and Native for App</p>
       </div>
 
     </>
