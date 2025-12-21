1bb417182d5ee7fd7c7fbd5e7b9b3e492e754f06
Kiran H K <kiranhkkiranhk60@gmail.com>
2025-12-21 20:44:57 +0530

    pitch

--- portfolio/src/home.jsx
+++ portfolio/src/home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import './App.css';
+
 
 export default function home() {
   return (
     <>
       <div style={{display:'flex', justifyContent:'center', height:'90vh', fontSize:'24px', fontWeight:'500'}}>      
-        <p>Hi, I'm Kiran H K 👋 </p>
+        <p id='home_para1'>Hi, I'm Kiran H K <p id='icon1'>👋</p> </p>
         <p>Full Stack web/app Developer</p>
         <p>Using React for Web and Native for App</p>
       </div>
-
+      <div id='ball'></div>
     </>
   )
 }
