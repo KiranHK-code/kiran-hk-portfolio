0463df3328c34b4760cc804c2de75c359e46780d
Kiran H K <kiranhkkiranhk60@gmail.com>
2025-12-25 19:59:14 +0530

    image

--- portfolio/src/home.jsx
+++ portfolio/src/home.jsx
@@ -5,7 +5,13 @@ import './App.css';
 export default function home() {
   return (
     <>
-      <div id='ball'></div>
+      <div className='home-container'>
+        <div className='home-text'>
+          <h2 id='head1'>Hello, I'm Kiran H K</h2>
+          <p id='para1'>Welcome to my portfolio! I'm passionate about creating beautiful and functional web applications. Explore my projects and skills below.</p>
+        </div>
+        <div className=''></div>
+      </div>
     </>
   )
 }
