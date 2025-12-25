48dc72a17b2aa1e04a8d263e8bdeda76b2c0c18a
Kiran H K <kiranhkkiranhk60@gmail.com>
2025-12-25 20:17:29 +0530

    developing image

--- portfolio/src/home.jsx
+++ portfolio/src/home.jsx
@@ -10,7 +10,8 @@ export default function home() {
           <h2 id='head1'>Hello, I'm Kiran H K</h2>
           <p id='para1'>Welcome to my portfolio! I'm passionate about creating beautiful and functional web applications. Explore my projects and skills below.</p>
         </div>
-        <div className=''></div>
+        <div className='image'>
+        </div>
       </div>
     </>
   )
